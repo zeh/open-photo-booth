876e16d87cf6dd66e82c2ff479d6dcfbc1d7ca56
zeh <zeh@zehfernando.com>
2015-02-16 16:40:55 -0500

    Rebuilt

--- release/js/photobooth.js
+++ release/js/photobooth.js
@@ -1,67 +1,158 @@
 var PhotoBooth;
 (function (PhotoBooth) {
-    var CameraView = (function () {
-        function CameraView() {
-            this.element = document.createElement("div");
-            this.element.style.width = "100%";
-            this.element.style.height = "100%";
-            this.element.style.overflow = "hidden";
-            this.video = document.createElement("video");
-            this.video.play();
-            this.video.autoplay = true;
-            this.video.style.width = "100%";
-            this.video.style.height = "100%";
-            this.video.style["objectFit"] = "cover";
-            this.element.appendChild(this.video);
+    var FilterDefinition = (function () {
+        function FilterDefinition(shader, vignetteOpacity, grainOpacity) {
+            this.shader = shader;
+            this.vignetteOpacity = vignetteOpacity;
+            this.grainOpacity = grainOpacity;
+        }
+        return FilterDefinition;
+    })();
+    PhotoBooth.FilterDefinition = FilterDefinition;
+    var FilterFactory = (function () {
+        function FilterFactory() {
+        }
+        FilterFactory._constructor = (function () {
+            FilterFactory.filters = new Array();
+            FilterFactory.filters.push(new FilterDefinition(new PIXI.SepiaFilter(), 0.5, 0.5));
+            var mtx = new PIXI.ColorMatrixFilter();
+            mtx.matrix = [
+                0.2,
+                0.6,
+                0.2,
+                0,
+                0.2,
+                0.6,
+                0.2,
+                0,
+                0.2,
+                0.6,
+                0.2,
+                0,
+                0,
+                0,
+                0,
+                1
+            ];
+            FilterFactory.filters.push(new FilterDefinition(mtx, 0.5, 0.5));
+        })();
+        return FilterFactory;
+    })();
+    PhotoBooth.FilterFactory = FilterFactory;
+})(PhotoBooth || (PhotoBooth = {}));
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var PhotoBooth;
+(function (PhotoBooth) {
+    var CameraSprite = (function (_super) {
+        __extends(CameraSprite, _super);
+        function CameraSprite(desiredWidth, desiredHeight) {
+            _super.call(this);
+            this.desiredWidth = desiredWidth;
+            this.desiredHeight = desiredHeight;
             if (this.hasGetUserMedia()) {
-                this.getUserMedia({ video: true, audio: false }, this.onLocalStreamCapture.bind(this), this.onLocalStreamError.bind(this));
+                this.getUserMedia({
+                    video: {
+                        mandatory: {
+                            maxWidth: 270,
+                            minHeight: 480
+                        },
+                        optional: [
+                            { height: 650 },
+                            { aspectRatio: 1 / 2 },
+                        ]
+                    }
+                }, this.onLocalStreamCapture.bind(this), this.onLocalStreamError.bind(this));
             }
             else {
                 console.error("getUserMedia() is not supported.");
             }
         }
-        CameraView.prototype.getElement = function () {
-            return this.element;
+        CameraSprite.prototype.createVideoElement = function () {
+            console.log("Creating video");
+            this.video = document.createElement("video");
+            this.video.autoplay = true;
+            this.video.play();
+            this.video.src = URL.createObjectURL(this.localMediaStream);
+            this.video.onloadedmetadata = this.onVideoLoadedMetaData.bind(this);
         };
-        CameraView.prototype.getUserMediaWrapper = function () {
+        CameraSprite.prototype.createVideoSprite = function () {
+            console.log("Creating texture");
+            this.videoTexture = PIXI.VideoTexture.textureFromVideo(this.video, PIXI.scaleModes.LINEAR);
+            this.videoSprite = new PIXI.Sprite(this.videoTexture);
+            this.addChild(this.videoSprite);
+            this.videoSprite.scale.x = this.desiredWidth / this.video.videoWidth;
+            this.videoSprite.scale.y = this.desiredHeight / this.video.videoHeight;
+            this.videoSprite.click = this.onClickCanvas.bind(this);
+            this.videoSprite.interactive = true;
+            this.currentFilterTemp = 0;
+            this.videoSprite.shader = PhotoBooth.FilterFactory.filters[0].shader;
+        };
+        CameraSprite.prototype.getUserMediaWrapper = function () {
             var func = navigator["getUserMedia"] || navigator["webkitGetUserMedia"] || navigator["mozGetUserMedia"] || navigator["msGetUserMedia"];
             if (func != undefined)
                 func = func.bind(navigator);
             return func;
         };
-        CameraView.prototype.hasGetUserMedia = function () {
+        CameraSprite.prototype.hasGetUserMedia = function () {
             return this.getUserMediaWrapper() != undefined;
         };
-        CameraView.prototype.getUserMedia = function (constraints, successCallback, errorCallback) {
+        CameraSprite.prototype.getUserMedia = function (constraints, successCallback, errorCallback) {
             var getUserMediaFunc = this.getUserMediaWrapper();
             if (getUserMediaFunc != undefined) {
                 getUserMediaFunc(constraints, successCallback, errorCallback);
             }
         };
-        CameraView.prototype.onLocalStreamError = function (e) {
-            console.error("Error capturing camera");
+        CameraSprite.prototype.onLocalStreamError = function (e) {
+            console.error("Error capturing camera: ", e);
         };
-        CameraView.prototype.onLocalStreamCapture = function (localMediaStream) {
+        CameraSprite.prototype.onLocalStreamCapture = function (localMediaStream) {
             console.log("Camera capture success");
-            this.video.src = URL.createObjectURL(localMediaStream);
-            this.video.onloadedmetadata = function (e) {
-                console.log("Loaded metadata.");
-            };
+            this.localMediaStream = localMediaStream;
+            this.createVideoElement();
         };
-        return CameraView;
-    })();
-    PhotoBooth.CameraView = CameraView;
+        CameraSprite.prototype.onVideoLoadedMetaData = function (e) {
+            console.log("Loaded metadata.");
+            console.log("Actual video size is " + this.video.videoWidth + "x" + this.video.videoHeight);
+            this.createVideoSprite();
+        };
+        CameraSprite.prototype.onClickCanvas = function (e) {
+            console.log("tap");
+            this.videoSprite.shader = PhotoBooth.FilterFactory.filters[(this.currentFilterTemp++) % PhotoBooth.FilterFactory.filters.length].shader;
+        };
+        return CameraSprite;
+    })(PIXI.DisplayObjectContainer);
+    PhotoBooth.CameraSprite = CameraSprite;
 })(PhotoBooth || (PhotoBooth = {}));
 var PhotoBooth;
 (function (PhotoBooth) {
     var App = (function () {
-        function App(root) {
-            this.root = root;
-            this.camera = new PhotoBooth.CameraView();
-            this.root.appendChild(this.camera.getElement());
+        function App(rootElement, width, height) {
+            this.rootElement = rootElement;
+            this.width = width;
+            this.height = height;
+            this.renderer = new PIXI.WebGLRenderer(width, height);
+            this.rootElement.appendChild(this.renderer.view);
+            this.stage = new PIXI.Stage(0xff000000);
+            this.cameraSprite = new PhotoBooth.CameraSprite(width, height);
+            this.stage.addChild(this.cameraSprite);
+            this.videoTexture = null;
+            var text = new PIXI.Text("TEST This is a test.");
+            text.x = 100;
+            text.y = 100;
+            this.stage.addChild(text);
+            this.render();
         }
         App.prototype.start = function () {
             console.log("Initialized successfully");
+        };
+        App.prototype.render = function () {
+            this.renderer.render(this.stage);
+            window.requestAnimationFrame(this.render.bind(this));
         };
         return App;
     })();
@@ -69,7 +160,7 @@ var PhotoBooth;
 })(PhotoBooth || (PhotoBooth = {}));
 window.addEventListener("load", function () {
     var element = document.getElementById('content');
-    var photobooth = new PhotoBooth.App(element);
+    var photobooth = new PhotoBooth.App(element, window.outerWidth, window.outerHeight);
     photobooth.start();
     var stats = new Stats();
     stats.setMode(0);
@@ -77,10 +168,11 @@ window.addEventListener("load", function () {
     stats.domElement.style.left = '0px';
     stats.domElement.style.top = '0px';
     document.body.appendChild(stats.domElement);
-    setInterval(function () {
+    var renderStats = function () {
         stats.begin();
         stats.end();
-    }, 1000 / 60);
-    var renderer = new PIXI.WebGLRenderer(800, 600);
+        window.requestAnimationFrame(renderStats);
+    };
+    renderStats();
 });
 //# sourceMappingURL=photobooth.js.map
